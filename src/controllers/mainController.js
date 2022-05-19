7f8d63cd1a4e26132a666e561565c5472b13f683
Alejandrodlk <ale_165851@hotmail.com>
2022-05-19 15:24:39 -0300

    .

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -2,12 +2,14 @@ const fs = require('fs');
 const path = require('path');
 
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const productsJSON = fs.readFileSync(productsFilePath , "utf-8")
+const products = JSON.parse(productsJSON);
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const controller = {
 	index: (req, res) => {
+		
 		let productsVisited = products.filter(product => product.category === "visited")
 		let productsInSale = products.filter(product => product.category === "in-sale")
 		return res.render("index" , {
